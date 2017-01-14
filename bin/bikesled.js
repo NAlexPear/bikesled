a8587b3d56c3abcaf171e1f3f013772c655cbc44
Alex Pearson <apearson@five-startech.com>
2017-01-14 15:07:51 -0600

    Specify default actions for CLI

--- bin/bikesled.js
+++ bin/bikesled.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 var runner = require( "commander" );
+var bikesled = require( "../index.js" );
+
+function parseAction( action ){
+    if( bikesled[ action ] ){
+        bikesled[ action ]();
+    }
+    else{
+        console.log( "Please input a valid bikesled command!" );
+
+        runner.outputHelp();
+    }
+}
 
 runner
     .arguments( "<action>" )
@@ -8,7 +21,9 @@ runner
         "-c --configure <config>",
         "Specify path to .bikesledrc configuration file"
     )
-    .action(
-        ( action ) => console.log( `You asked bikesled to ${action}` )
-    )
+    .action( parseAction )
     .parse( process.argv );
+
+if( !process.argv.slice( 2 ).length ){
+    runner.outputHelp();
+}
